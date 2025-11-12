9785eaf0fc58bc0c653f019d1d9f262352063b30
Kirbass <kirillasz3@gmail.com>
2025-11-12 16:54:51 +0300

    Add inverse function

--- include/veekay/types.hpp
+++ include/veekay/types.hpp
@@ -412,6 +412,148 @@ union mat4 {
 		return result;
 	}
 
+	static mat4 inverse(const mat4& m) {
+		mat4 result{};
+		// Flatten to 1D array for easier formula usage: a[row*4 + col]
+		float a[16];
+		for (int r = 0; r < 4; ++r)
+			for (int c = 0; c < 4; ++c)
+				a[r * 4 + c] = m.elements[r][c];
+
+		float inv[16];
+
+		inv[0] = a[5]  * a[10] * a[15] -
+				a[5]  * a[11] * a[14] -
+				a[9]  * a[6]  * a[15] +
+				a[9]  * a[7]  * a[14] +
+				a[13] * a[6]  * a[11] -
+				a[13] * a[7]  * a[10];
+
+		inv[4] = -a[4]  * a[10] * a[15] +
+				a[4]  * a[11] * a[14] +
+				a[8]  * a[6]  * a[15] -
+				a[8]  * a[7]  * a[14] -
+				a[12] * a[6]  * a[11] +
+				a[12] * a[7]  * a[10];
+
+		inv[8] = a[4]  * a[9] * a[15] -
+				a[4]  * a[11] * a[13] -
+				a[8]  * a[5] * a[15] +
+				a[8]  * a[7] * a[13] +
+				a[12] * a[5] * a[11] -
+				a[12] * a[7] * a[9];
+
+		inv[12] = -a[4]  * a[9] * a[14] +
+				a[4]  * a[10] * a[13] +
+				a[8]  * a[5] * a[14] -
+				a[8]  * a[6] * a[13] -
+				a[12] * a[5] * a[10] +
+				a[12] * a[6] * a[9];
+
+		inv[1] = -a[1]  * a[10] * a[15] +
+				a[1]  * a[11] * a[14] +
+				a[9]  * a[2] * a[15] -
+				a[9]  * a[3] * a[14] -
+				a[13] * a[2] * a[11] +
+				a[13] * a[3] * a[10];
+
+		inv[5] = a[0]  * a[10] * a[15] -
+				a[0]  * a[11] * a[14] -
+				a[8]  * a[2] * a[15] +
+				a[8]  * a[3] * a[14] +
+				a[12] * a[2] * a[11] -
+				a[12] * a[3] * a[10];
+
+		inv[9] = -a[0]  * a[9] * a[15] +
+				a[0]  * a[11] * a[13] +
+				a[8]  * a[1] * a[15] -
+				a[8]  * a[3] * a[13] -
+				a[12] * a[1] * a[11] +
+				a[12] * a[3] * a[9];
+
+		inv[13] = a[0]  * a[9] * a[14] -
+				a[0]  * a[10] * a[13] -
+				a[8]  * a[1] * a[14] +
+				a[8]  * a[2] * a[13] +
+				a[12] * a[1] * a[10] -
+				a[12] * a[2] * a[9];
+
+		inv[2] = a[1]  * a[6] * a[15] -
+				a[1]  * a[7] * a[14] -
+				a[5]  * a[2] * a[15] +
+				a[5]  * a[3] * a[14] +
+				a[13] * a[2] * a[7] -
+				a[13] * a[3] * a[6];
+
+		inv[6] = -a[0]  * a[6] * a[15] +
+				a[0]  * a[7] * a[14] +
+				a[4]  * a[2] * a[15] -
+				a[4]  * a[3] * a[14] -
+				a[12] * a[2] * a[7] +
+				a[12] * a[3] * a[6];
+
+		inv[10] = a[0]  * a[5] * a[15] -
+				a[0]  * a[7] * a[13] -
+				a[4]  * a[1] * a[15] +
+				a[4]  * a[3] * a[13] +
+				a[12] * a[1] * a[7] -
+				a[12] * a[3] * a[5];
+
+		inv[14] = -a[0]  * a[5] * a[14] +
+				a[0]  * a[6] * a[13] +
+				a[4]  * a[1] * a[14] -
+				a[4]  * a[2] * a[13] -
+				a[12] * a[1] * a[6] +
+				a[12] * a[2] * a[5];
+
+		inv[3] = -a[1] * a[6] * a[11] +
+				a[1] * a[7] * a[10] +
+				a[5] * a[2] * a[11] -
+				a[5] * a[3] * a[10] -
+				a[9] * a[2] * a[7] +
+				a[9] * a[3] * a[6];
+
+		inv[7] = a[0] * a[6] * a[11] -
+				a[0] * a[7] * a[10] -
+				a[4] * a[2] * a[11] +
+				a[4] * a[3] * a[10] +
+				a[8] * a[2] * a[7] -
+				a[8] * a[3] * a[6];
+
+		inv[11] = -a[0] * a[5] * a[11] +
+				a[0] * a[7] * a[9] +
+				a[4] * a[1] * a[11] -
+				a[4] * a[3] * a[9] -
+				a[8] * a[1] * a[7] +
+				a[8] * a[3] * a[5];
+
+		inv[15] = a[0] * a[5] * a[10] -
+				a[0] * a[6] * a[9] -
+				a[4] * a[1] * a[10] +
+				a[4] * a[2] * a[9] +
+				a[8] * a[1] * a[6] -
+				a[8] * a[2] * a[5];
+
+		float det = a[0] * inv[0] + a[1] * inv[4] + a[2] * inv[8] + a[3] * inv[12];
+
+		if (fabsf(det) < 1e-9f) {
+			// singular matrix: return identity (or handle error as you prefer)
+			return mat4::identity();
+		}
+
+		float invDet = 1.0f / det;
+		for (int i = 0; i < 16; ++i)
+			inv[i] *= invDet;
+
+		// write back to result.elements[row][col]
+		for (int r = 0; r < 4; ++r)
+			for (int c = 0; c < 4; ++c)
+				result.elements[r][c] = inv[r * 4 + c];
+
+		return result;
+	}
+
+
 	mat4 operator*(const mat4& other) const {
 		mat4 result{};
 
